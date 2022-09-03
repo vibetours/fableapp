7400f04608e2866de504427f2ab8917466ccb1e0
akash <akash@sharefable.com>
2022-09-03 09:27:31 +0000

    Gets redux devtools to work with state

--- workspaces/packages/client/src/store_config.ts
+++ workspaces/packages/client/src/store_config.ts
@@ -1,5 +1,6 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./reducer";
 import thunk from "redux-thunk";
 
-export default createStore(rootReducer, applyMiddleware(thunk));
+const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+export default createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
