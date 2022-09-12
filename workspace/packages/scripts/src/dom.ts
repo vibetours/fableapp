8b7df7aa39ae1706fc75e0e35deaab94af76c5be
akash <akash@sharefable.com>
2022-09-12 17:25:10 +0000

    Adds icon on hover

--- workspace/packages/scripts/src/dom.ts
+++ workspace/packages/scripts/src/dom.ts
@@ -1,6 +1,22 @@
+import { render } from "eta";
+import HOVER_IND from "./views/hover_indicator";
+
+function getRandomNo(): string {
+  const msStr = (+new Date()).toString();
+  return msStr.substring(4) + ((Math.random() * 10000) | 0);
+}
+
+/*
+ * When a property value is set via this script, old property value is stored
+ * as part of the dom object property with a default value.
+ * Like if we are changing the border of an HTMLElement then old value of el.style.border
+ * is saved as el.__fab_style_border__
+ * This is required as often time we dynmically change property value of an dom and we restore
+ * it back later
+ */
 function saveCurrentProperty(el: any, propertyName: string, defaultValue: any) {
-  const props = propertyName.split('.');
-  const fullPropName = `__fab_${props.join('_')}__`;
+  const props = propertyName.split(".");
+  const fullPropName = `__fab_${props.join("_")}__`;
   let e = el;
   for (const prop of props) {
     if (e !== null && e !== undefined && prop in e) {
@@ -14,9 +30,13 @@ function saveCurrentProperty(el: any, propertyName: string, defaultValue: any) {
   el[fullPropName] = e;
 }
 
+/*
+ * After the property value is changed some ops is performed, we restore the old property value
+ * back that was stored earlier. In that case we pass (el, 'style.border') to restore the prev value
+ */
 function restoreSavedProperty(el: any, propertyName: string) {
-  const props = propertyName.split('.');
-  const fullPropName = `__fab_${props.join('_')}__`;
+  const props = propertyName.split(".");
+  const fullPropName = `__fab_${props.join("_")}__`;
   let e = el;
   for (let i = 0; i < props.length - 1; i++) {
     const prop = props[i];
@@ -33,39 +53,145 @@ function restoreSavedProperty(el: any, propertyName: string) {
   delete el[fullPropName];
 }
 
+/*
+ * Contains the current element selection of DOM.
+ */
+class DOMElementSelection {
+  currentEl: HTMLElement | null = null;
+
+  lastEl: HTMLElement | null = null;
+
+  clear = () => {
+    this.currentEl = null;
+    this.lastEl = null;
+  };
+
+  from(that: DOMElementSelection) {
+    this.currentEl = that.currentEl;
+    this.lastEl = that.currentEl;
+  }
+}
+
+enum SelectionRegistryType {
+  // When mouse is hovered to see outline of dom elements. This happens when edit button is clicked
+  // and an item is not pinned
+  PROBING = "PROBING",
+}
+
+const SELECTION_REGISTRY: Record<SelectionRegistryType, DOMElementSelection> = {
+  [SelectionRegistryType.PROBING]: new DOMElementSelection(),
+};
+
+/*
+ * Container to show extra information around a bounding box
+ */
+class FollowBehindContainer {
+  private doc: HTMLDocument;
+
+  private con: HTMLDivElement;
+
+  private rect: DOMRect;
+
+  constructor(doc: HTMLDocument) {
+    this.doc = doc;
+
+    this.con = this.doc.createElement("div");
+    this.con.style.position = "absolute";
+    this.con.innerHTML = render(HOVER_IND, {}) as string;
+    this.con.setAttribute("id", `fab-fbc-${getRandomNo()}`);
+    this.con.onmouseover = (e) => {
+      (e as any).__fab__target = this.con;
+      e.stopPropagation();
+    };
+    this.con.onmouseout = (e) => {
+      (e as any).__fab__target = this.con;
+      e.stopPropagation();
+    };
+    this.con.onclick = (e) => {
+      e.stopPropagation();
+    };
+    this.con.style.fontSize = "12px";
+    this.con.style.zIndex = "-1";
+    this.con.style.background = "#424242";
+    this.con.style.color = "#fff";
+    this.con.style.padding = "4px 6px";
+    this.doc.body.appendChild(this.con);
+
+    this.rect = this.con.getBoundingClientRect();
+    this.moveOutsideViewPort();
+  }
+
+  isPresentInPath(els: Array<HTMLElement>) {
+    for (const el of els) {
+      if (el === this.con) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  moveOutsideViewPort() {
+    this.con.style.display = "none";
+  }
+
+  bringInViewPort(anchorEl: HTMLStyleElement) {
+    const rect = anchorEl.getBoundingClientRect();
+    this.con.style.left = `${rect.left}px`;
+    this.con.style.top = `${rect.top - this.rect.height + 2}px`;
+    this.con.style.display = "block";
+    this.con.style.zIndex = "50";
+  }
+
+  destroy() {
+    this.doc.body.removeChild(this.con);
+  }
+}
+
 export const pointDOMElsWhenHovered: {
-  __lastEl: HTMLElement | null;
+  __followBehind: FollowBehindContainer | null;
   reg: (doc: HTMLDocument) => void;
   unreg: (doc: HTMLDocument) => void;
 } = {
-  __lastEl: null,
-
+  __followBehind: null,
   reg: (doc: HTMLDocument) => {
-    saveCurrentProperty(doc.body, 'onmousemove', null);
+    const that = pointDOMElsWhenHovered;
+    saveCurrentProperty(doc.body, "onmousemove", null);
+    that.__followBehind = new FollowBehindContainer(doc);
     doc.body.onmousemove = function (e) {
-      const that = pointDOMElsWhenHovered;
-      const el = doc.elementFromPoint(e.clientX, e.clientY) as HTMLStyleElement;
+      const lastEl = SELECTION_REGISTRY.PROBING.lastEl;
+      const els = doc.elementsFromPoint(e.clientX, e.clientY) as Array<HTMLStyleElement>;
+      if (that.__followBehind?.isPresentInPath(els)) {
+        return;
+      }
+      const el = els[0];
       if (el) {
-        if (el !== that.__lastEl) {
-          if (that.__lastEl) {
-            restoreSavedProperty(that.__lastEl, 'style.boxShadow');
+        if (el !== lastEl) {
+          SELECTION_REGISTRY.PROBING.currentEl = el;
+          if (lastEl) {
+            restoreSavedProperty(lastEl, "style.boxShadow");
           }
           if (el.style) {
-            saveCurrentProperty(el, 'style.boxShadow', 'initial');
-            el.style.boxShadow = 'inset 0px 0px 0px 2px black';
+            that.__followBehind?.bringInViewPort(el);
+            saveCurrentProperty(el, "style.boxShadow", "initial");
+            el.style.boxShadow = "inset 0px 0px 0px 2px #424242";
           }
         }
-        that.__lastEl = el;
+        SELECTION_REGISTRY.PROBING.lastEl = el;
       }
     };
   },
-  unreg: (doc: HTMLDocument) => {
-    const that = pointDOMElsWhenHovered;
-    if (that.__lastEl) {
-      restoreSavedProperty(that.__lastEl, 'style.boxShadow');
+  unreg: (doc: HTMLDocument, keepSelection = false) => {
+    const lastEl = SELECTION_REGISTRY.PROBING.lastEl;
+    if (lastEl) {
+      restoreSavedProperty(lastEl, "style.boxShadow");
     }
-    restoreSavedProperty(doc.body, 'onmousemove');
-    restoreSavedProperty(doc.body, 'onmouseout');
+    !keepSelection && SELECTION_REGISTRY.PROBING.clear();
+    const that = pointDOMElsWhenHovered;
+    if (that.__followBehind) {
+      that.__followBehind.destroy();
+    }
+    restoreSavedProperty(doc.body, "onmousemove");
+    restoreSavedProperty(doc.body, "onmouseout");
   },
 };
 
---
+++ workspace/packages/scripts/src/views/hover_indicator.ts
@@ -0,0 +1,29 @@
+/* eslint-disable max-len */
+
+const html = `
+<div style="display: flex; flex-direction: row;">
+  <div style="width: 14px; height: 16px;">
+    <?xml version="1.0" encoding="iso-8859-1"?>
+    <svg version="1.1" 
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+      viewBox="0 0 512 512" style="fill:#fff;" xml:space="preserve">
+        <g>
+          <g>
+            <path d="M458.667,0H53.333c-5.867,0-10.667,4.8-10.667,10.667v64c0,5.867,4.8,10.667,10.667,10.667
+              C59.2,85.333,64,80.533,64,74.667V21.333h181.333v469.333h-64c-5.867,0-10.667,4.8-10.667,10.667
+              c0,5.867,4.8,10.667,10.668,10.667h149.333c5.867,0,10.667-4.8,10.667-10.667c0-5.867-4.8-10.667-10.667-10.667h-64V21.333H448
+              v53.333c0,5.867,4.8,10.667,10.667,10.667c5.867,0,10.667-4.8,10.667-10.667v-64C469.333,4.8,464.533,0,458.667,0z"/>
+          </g>
+        </g>
+      </svg>
+  </div>
+  <div style="width: 14px; height: 16px; margin-left: 10px;">
+    <svg viewBox="0 0 30 30" xmlns="http://www.w3.org/2000/svg" style="fill:#fff;">
+      <path id="Path_245" data-name="Path 245" d="M31,4a3,3,0,0,0-3-3H15.59a3,3,0,0,0-3,3V16.41a3,3,0,0,0,3,3H28a3,3,0,0,0,3-3ZM29,4V16.41a1,1,0,0,1-1,1H15.59a1,1,0,0,1-1-1V4a1,1,0,0,1,1-1H28A1,1,0,0,1,29,4Z" transform="translate(-1 -1)" fill-rule="evenodd"/>
+      <path id="Path_246" data-name="Path 246" d="M10.4,12.59H4a3,3,0,0,0-3,3V28a3,3,0,0,0,3,3H16.41a3,3,0,0,0,3-3V21.6a1,1,0,1,0-2,0V28a1,1,0,0,1-1,1H4a1,1,0,0,1-1-1V15.59a1,1,0,0,1,1-1h6.4a1,1,0,0,0,0-2Z" transform="translate(-1 -1)" fill-rule="evenodd"/>
+    </svg>
+  </div>
+</div>
+`;
+export default html;
