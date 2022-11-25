2a92aadd977ba12d66de065f40ce4df28cd1af08
akash <akash@sharefable.com>
2022-11-25 08:14:01 +0000

    Linting fixes

--- workspace/packages/ext-tour/webpack.config.js
+++ workspace/packages/ext-tour/webpack.config.js
@@ -1,7 +1,7 @@
-const path = require('path');
-const webpack = require('webpack');
-const CopyPlugin = require('copy-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const CopyPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const envKeys = Object.keys(process.env).reduce((prev, next) => {
   prev[`process.env.${next}`] = JSON.stringify(process.env[next]);
@@ -10,27 +10,27 @@ const envKeys = Object.keys(process.env).reduce((prev, next) => {
 
 const config = {
   entry: {
-    background: './src/background.ts',
-    popup: './src/popup/index.tsx',
+    background: "./src/background.ts",
+    popup: "./src/popup/index.tsx",
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /(node_modules)/,
-        loader: 'babel-loader',
-        options: { presets: ['@babel/env'] },
+        loader: "babel-loader",
+        options: { presets: ["@babel/env"] },
       },
       {
         test: /\.(ts|tsx)$/,
         exclude: /(node_modules)/,
         use: [
           {
-            loader: 'babel-loader',
-            options: { presets: ['@babel/env'] },
+            loader: "babel-loader",
+            options: { presets: ["@babel/env"] },
           },
           {
-            loader: 'ts-loader',
+            loader: "ts-loader",
           },
         ],
       },
@@ -40,33 +40,33 @@ const config = {
           {
             loader: MiniCssExtractPlugin.loader,
           },
-          'css-loader',
-          'less-loader',
+          "css-loader",
+          "less-loader",
         ],
       },
     ],
   },
-  resolve: { extensions: ['.ts', '.tsx', '.js', '.jsx'] },
+  resolve: { extensions: [".ts", ".tsx", ".js", ".jsx"] },
   output: {
-    path: path.resolve(__dirname, 'build/'),
-    filename: '[name].js',
+    path: path.resolve(__dirname, "build/"),
+    filename: "[name].js",
   },
   plugins: [
     new CopyPlugin({
-      patterns: [{ from: 'public', to: './' }]
+      patterns: [{ from: "public", to: "./" }]
     }),
     new MiniCssExtractPlugin({
-      filename: '[name].css',
+      filename: "[name].css",
     }),
     new webpack.DefinePlugin(envKeys),
   ],
 };
 
-if (process.env.NODE_ENV === 'production') {
-  config.mode = 'production';
+if (process.env.NODE_ENV === "production") {
+  config.mode = "production";
 } else {
-  config.mode = 'development';
-  config.devtool = 'inline-source-map';
+  config.mode = "development";
+  config.devtool = "inline-source-map";
 }
 
 module.exports = config;
